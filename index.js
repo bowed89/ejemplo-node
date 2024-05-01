0bd4daeffca629dede2dc6b89dd7e90708b77f52
bowed89 <jesus.lopez.averanga@gmail.com>
2024-05-01 19:53:00 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = 4000;
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
